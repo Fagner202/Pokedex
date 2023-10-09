c84562a3d91856825e6ef42cbb821b6afcab0883
Fagner Joaquim <131827373+Fagner2002@users.noreply.github.com>
2023-10-08 23:31:26 -0300

    Card
    Construindo cards dos pokemos

--- App/script.js
+++ App/script.js
@@ -20,3 +20,47 @@ $(document).ready(function() {
         }
     });
 });
+
+$(document).ready(function () {
+    const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+    // Função para buscar dados da API e criar os cards
+    function fetchAndDisplayPokemon() {
+        $.get(apiUrl, function (data) {
+            const pokemonList = data.results;
+
+            // Limpar o conteúdo existente na div de cards
+            $("#pokemon-cards").empty();
+
+            // Iterar sobre a lista de Pokémon e criar os cards
+            $.each(pokemonList, function (index, pokemon) {
+                const pokemonUrl = pokemon.url;
+
+                // Buscar dados detalhados do Pokémon
+                $.get(pokemonUrl, function (pokemonData) {
+                    const name = pokemonData.name;
+                    const imageUrl = pokemonData.sprites.front_default;
+                    const tipo = pokemonData.types;
+                    console.log(pokemonData);
+                    console.log(tipo);
+
+                    // Criar o card do Pokémon
+                    const card = `
+                        <div class="card" style="width: 13rem;">
+                            <img src="${imageUrl}" class="card-img-top" alt="${name}">
+                            <div class="card-body">
+                                <h5 class="card-title">${name}</h5>
+                            </div>
+                        </div>
+                    `;
+
+                    // Adicionar o card à div de cards
+                    $("#pokemon-cards").append(card);
+                });
+            });
+        });
+    }
+
+    // Chamar a função para buscar e exibir os cards
+    fetchAndDisplayPokemon();
+});
