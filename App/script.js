2db448c338e505a66a1cf6ebdcd47e74955591e4
Fagner Joaquim <131827373+Fagner2002@users.noreply.github.com>
2023-10-09 21:07:54 -0300

    aplicando cores nos tipos

--- App/script.js
+++ App/script.js
@@ -24,13 +24,65 @@ $(document).ready(function() {
 $(document).ready(function () {
     const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
+    // Função para mapear os tipos para classes CSS correspondentes
+    function mapTypeToCssClass(type) {
+        switch (type) {
+            case "fire":
+                return "fire-type";
+            case "grass":
+                return "grass-type";
+            case "poison":
+                return "poison-type";
+            case "flying":
+                return "flying-type";
+            case "bug":
+                return "bug-type";
+            case "water":
+                return "water-type";
+            case "normal":
+                return "normal-type";
+            case "electric":
+                return "electric-type";
+            case "ground":
+                return "ground-type"
+            case "fairy":
+                return "fairy-type"
+            case "ice":
+                return "ice-type"
+            case "psychic":
+                return "psychic-type"
+            case "fighting":
+                return "fighting-type"
+            case "steel":
+                return "steel-type"
+            case "ghost":
+                return "ghost-type";
+            case "rock":
+                return "rock-type"
+            case "dragon":
+                return "dragon-type"
+            default:
+                return "";
+        }
+    }
+
     // Função para buscar dados da API e criar os cards
     function fetchAndDisplayPokemon() {
-        $.get(apiUrl, function (data) {
+        // Atualizar a URL da API para buscar apenas os 151 primeiros Pokémon
+        const apiUrl151 = `${apiUrl}?limit=151`;
+
+        $.get(apiUrl151, function (data) {
             const pokemonList = data.results;
 
             // Limpar o conteúdo existente na div de cards
             $("#pokemon-cards").empty();
+
+            // Ordenar a lista de Pokémon por ID
+            pokemonList.sort(function (a, b) {
+                const idA = a.url.split("/").slice(-2, -1)[0];
+                const idB = b.url.split("/").slice(-2, -1)[0];
+                return idA - idB;
+            });
 
             // Iterar sobre a lista de Pokémon e criar os cards
             $.each(pokemonList, function (index, pokemon) {
@@ -40,13 +92,18 @@ $(document).ready(function () {
                 $.get(pokemonUrl, function (pokemonData) {
                     const name = pokemonData.name;
                     const imageUrl = pokemonData.sprites.front_default;
-                    const tipo = pokemonData.types;
-                    console.log(pokemonData);
-                    console.log(tipo);
+                    const types = pokemonData.types.map(function (typeData) {
+                        return typeData.type.name;
+                    });
+                    console.log(pokemonData.id);
 
-                    // Criar o card do Pokémon
+                    // Criar o card do Pokémon com classes CSS baseadas nos tipos
                     const card = `
                         <div class="card" style="width: 13rem;">
+                            ${types.map(function (type) {
+                                const cssClass = mapTypeToCssClass(type);
+                                return `<p class="card-type ${cssClass}">${type}</p>`;
+                            }).join("")}
                             <img src="${imageUrl}" class="card-img-top" alt="${name}">
                             <div class="card-body">
                                 <h5 class="card-title">${name}</h5>
